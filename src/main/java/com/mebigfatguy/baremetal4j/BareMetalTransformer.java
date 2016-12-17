e58332e9e188dc107854976a28fb5a4602ff9765
Dave Brosius <dbrosius@mebigfatguy.com>
2016-12-17 09:26:51 -0500

    add basic agent structure

---
+++ src/main/java/com/mebigfatguy/baremetal4j/BareMetalTransformer.java
@@ -0,0 +1,50 @@
+/*
+ * baremetal4j - A java aspect for allowing debugging at the byte code level from source debuggers (as in IDEs)
+ * Copyright 2016 MeBigFatGuy.com
+ * Copyright 2016 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.baremetal4j;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+
+public class BareMetalTransformer implements ClassFileTransformer {
+
+    private Options options;
+
+    public BareMetalTransformer(Options options) {
+        this.options = options;
+    }
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer)
+            throws IllegalClassFormatException {
+
+        if (className.startsWith("java/") || className.startsWith("javax/") || className.startsWith("sun/")
+                || className.startsWith("com/mebigfatguy/baremetal4j") || !options.instrumentClass(className)) {
+            return classfileBuffer;
+        }
+
+        // temporary
+        return classfileBuffer;
+
+    }
+
+    @Override
+    public String toString() {
+        return ToString.build(this);
+    }
+}
---
+++ src/main/java/com/mebigfatguy/baremetal4j/Options.java
@@ -0,0 +1,86 @@
+/*
+ * baremetal4j - A java aspect for allowing debugging at the byte code level from source debuggers (as in IDEs)
+ * Copyright 2016 MeBigFatGuy.com
+ * Copyright 2016 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.baremetal4j;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Options {
+
+    private static final String APPLY_ALL_NAME = "apply_all";
+    private static final String EXCLUSION_PATTERN_NAME = "exclusion_pattern";
+    private static final String INCLUSION_PATTERN_NAME = "inclusion_pattern";
+
+    private boolean applyAll;
+    private Pattern classExclusionPattern;
+    private Pattern classInclusionPattern;
+
+    public Options(String agentArgs) {
+        if (agentArgs != null) {
+            String[] args = agentArgs.split(";");
+            for (String arg : args) {
+                try {
+                    String[] kv = arg.split("=");
+                    String key = kv[0].trim();
+
+                    switch (key) {
+                        case APPLY_ALL_NAME:
+                            applyAll = Boolean.parseBoolean(kv[1].trim());
+                        break;
+
+                        case EXCLUSION_PATTERN_NAME:
+                            classExclusionPattern = Pattern.compile(kv[1].trim());
+                        break;
+
+                        case INCLUSION_PATTERN_NAME:
+                            classInclusionPattern = Pattern.compile(kv[1].trim());
+                        break;
+                    }
+                } catch (Exception e) {
+                    // swallow it
+                }
+            }
+        }
+    }
+
+    public boolean instrumentClass(String className) {
+
+        if (applyAll) {
+            return true;
+        }
+
+        boolean instrument = true;
+
+        if (classExclusionPattern != null) {
+            Matcher m = classExclusionPattern.matcher(className);
+            instrument = !m.matches();
+        }
+
+        if (classInclusionPattern != null) {
+            Matcher m = classInclusionPattern.matcher(className);
+            instrument = m.matches();
+        }
+
+        return instrument;
+    }
+
+    @Override
+    public String toString() {
+        return ToString.build(this);
+    }
+}
